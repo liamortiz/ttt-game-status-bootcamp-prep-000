6922cc138e5b39479a2210836b530c5fc2367f91
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-10 00:54:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,31 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0, 1, 2], # Top row
+                    [3, 4, 5], # Bottom row
+                    [6, 7, 8], # Last row
+                    [0, 3, 6], # Left col
+                    [1, 4, 7], # Middle col
+                    [2, 5, 8], # Right
+                    [0, 4, 8], # X1
+                    [6, 4, 2]  # X2
+                    ]
+def won?(board)
+  if !board.any?{|c| c == "X" || c == "Y"} or draw?(board)
+    return false
+  end
+  
+  WIN_COMBINATIONS.select{|row| row.all?{|index| position_taken?(board, index)}}.each do |row|
+    if row.all?{|index| board[index] == 'X'} or row.all?{|index| board[index] == 'O'}
+      return row
+    end
+  end
+end
+
+def full?(board)
+  !board.any?(" ")
+end
+
+def draw?(board)
+  return (not won?(board) or (board.count{|c| c == "O" or c == "X"} == 9))
+end
