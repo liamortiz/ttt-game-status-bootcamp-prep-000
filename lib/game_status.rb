ac65ddc10fb2c547eb533d76f87f9b8b3b7f4f49
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-10 01:45:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -33,7 +33,7 @@ def full?(board)
 end
 
 def draw?(board)
-  !won?(board) or board.count{|c| c == "O" or c == "X"} == 9
+  won?(board) or board.count{|c| c == "O" or c == "X"} == 9
 end
 
 def over?(board)
